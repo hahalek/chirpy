42f070ad6d197a288ad6a1a4652c24dfd75f13c8
hahalek <agata.machalek@gmail.com>
2026-06-04 10:28:29 +0200

    trying to shorten validateChirp func

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
-	const filepathRoot = "/home/hahalek/workspace/github.com/hahalek/chirpy"
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		requestsProcessed: atomic.Int32{},
+	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("GET /healthz", healthzHandler)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.getMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	server := &http.Server{
 		Handler: mux,
@@ -27,4 +36,89 @@ func healthzHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	requestsProcessed atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.requestsProcessed.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.requestsProcessed.Load())))
+
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.requestsProcessed.Swap(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type reqParameters struct {
+		Body string `json:"body"`
+	}
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	reqParam := reqParameters{}
+	err := decoder.Decode(&reqParam)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+	if len(reqParam.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	type respValues struct {
+		Valid bool `json:"valid"`
+	}
+	respParam := respValues{
+		Valid: true,
+	}
+
+	respondWithJSON(w, http.StatusOK, respParam)
+}
+
+/*
+	func decodeRequest(w http.ResponseWriter, r *http.Request, requestStructure *interface{}) {
+		defer r.Body.Close()
+
+		decoder := json.NewDecoder(r.Body)
+		reqParam := requestStructure
+		err := decoder.Decode(&reqParam)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Something went wrong")
+			return
+		}
+	}
+*/
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
 }
