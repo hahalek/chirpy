5fa94e156012d9aed91a8c841a64c71fdb314acb
hahalek <agata.machalek@gmail.com>
2026-06-07 13:46:52 +0200

    get chirps endpoing done

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		fmt.Printf(err.Error())
@@ -33,21 +34,16 @@ func main() {
 	apiCfg := apiConfig{
 		requestsProcessed: atomic.Int32{},
 		dbQ:               dbQueries,
-	}
-
-	type User struct {
-		ID        uuid.UUID `json:"id"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Email     string    `json:"email"`
+		platform:          platform,
 	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.getMetrics)
-	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
-	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetUsers)
 	mux.HandleFunc("POST /api/users", apiCfg.addUser)
+	mux.HandleFunc("POST /api/chirps", apiCfg.postChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.getAllChirps)
 
 	server := &http.Server{
 		Handler: mux,
@@ -56,6 +52,21 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func healthzHandler(w http.ResponseWriter, req *http.Request) {
@@ -67,6 +78,7 @@ func healthzHandler(w http.ResponseWriter, req *http.Request) {
 type apiConfig struct {
 	requestsProcessed atomic.Int32
 	dbQ               *database.Queries
+	platform          string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -87,8 +99,14 @@ func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.requestsProcessed.Swap(0)
+func (cfg *apiConfig) resetUsers(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte(http.StatusText(http.StatusForbidden)))
+		return
+	}
+	cfg.dbQ.DeleteAllUsers(r.Context())
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
@@ -100,43 +118,75 @@ func (cfg *apiConfig) addUser(w http.ResponseWriter, r *http.Request) {
 		Email string `json:"email"`
 	}
 	reqParam := requestStructure{}
-	decodeRequest(w, r, reqParam)
+	decodeRequest(w, r, &reqParam)
 	user, err := cfg.dbQ.CreateUser(r.Context(), reqParam.Email)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		respondWithError(w, http.StatusBadRequest, "Something went wrong1")
 		return
 	}
 
-	response := database.User{
+	response := User{
 		ID:        user.ID,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 		Email:     user.Email,
 	}
 	respondWithJSON(w, http.StatusCreated, response)
-
 }
 
-func validateChirp(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) postChirp(w http.ResponseWriter, r *http.Request) {
+	cleanedChirp, userID := validateChirp(w, r)
+
+	createChirpParams := database.CreateChirpParams{
+		Body:   cleanedChirp,
+		UserID: userID,
+	}
+
+	newChirp, _ := cfg.dbQ.CreateChirp(r.Context(), createChirpParams)
+
+	responseStructure := Chirp{
+		ID:        newChirp.ID,
+		CreatedAt: newChirp.CreatedAt,
+		UpdatedAt: newChirp.UpdatedAt,
+		Body:      newChirp.Body,
+		UserID:    newChirp.UserID,
+	}
+	respondWithJSON(w, http.StatusCreated, responseStructure)
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) (string, uuid.UUID) {
 	defer r.Body.Close()
 	type reqParameters struct {
-		Body string `json:"body"`
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
 	}
 	reqParam := reqParameters{}
 	decodeRequest(w, r, &reqParam)
 	if correct := checkChirtLength(w, reqParam.Body); correct == false {
-		return
+		return "", uuid.UUID{}
 	}
 	cleanedBody := checkChirpProfanity(w, reqParam.Body)
 
-	type respValues struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-	respParam := respValues{
-		CleanedBody: cleanedBody,
-	}
+	return cleanedBody, reqParam.UserID
+}
 
-	respondWithJSON(w, http.StatusOK, respParam)
+func (cfg *apiConfig) getAllChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.dbQ.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong2")
+	}
+	chirpArray := []Chirp{}
+	for _, chirp := range chirps {
+		c := Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		}
+		chirpArray = append(chirpArray, c)
+	}
+	respondWithJSON(w, http.StatusOK, chirpArray)
 }
 
 func decodeRequest(w http.ResponseWriter, r *http.Request, reqParam any) {
